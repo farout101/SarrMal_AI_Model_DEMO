4741bd89e910715bd48eb25f83acaf8e6c753fdc
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-08-22 23:34:44 +0630

    underdevelopment

--- testings/tempCodeRunnerFile.py
+++ testings/tempCodeRunnerFile.py
@@ -1,27 +1,27 @@
-"dinner": content.Schema(
-            type = content.Type.OBJECT,
-            properties = {
-              "main_dish": content.Schema(
-                type = content.Type.OBJECT,
-                enum = "[]",
-                required = """["name", "calories", "category", "ingredients"]""",
-                properties = {
-                  "name": content.Schema(
-                    type = content.Type.STRING,
-                  ),
-                  "calories": content.Schema(
-                    type = content.Type.NUMBER,
-                  ),
-                  "category": content.Schema(
-                    type = content.Type.STRING,
-                  ),
-                  "how_to_cook": content.Schema(
-                    type = content.Type.STRING,
-                  ),
-                  "meal_time": content.Schema(
-                    type = content.Type.STRING,
-                  ),
-                },
-              ),
-            },
-          ),
+# "dinner": content.Schema(
+#             type = content.Type.OBJECT,
+#             properties = {
+#               "main_dish": content.Schema(
+#                 type = content.Type.OBJECT,
+#                 enum = "[]",
+#                 required = """["name", "calories", "category", "ingredients"]""",
+#                 properties = {
+#                   "name": content.Schema(
+#                     type = content.Type.STRING,
+#                   ),
+#                   "calories": content.Schema(
+#                     type = content.Type.NUMBER,
+#                   ),
+#                   "category": content.Schema(
+#                     type = content.Type.STRING,
+#                   ),
+#                   "how_to_cook": content.Schema(
+#                     type = content.Type.STRING,
+#                   ),
+#                   "meal_time": content.Schema(
+#                     type = content.Type.STRING,
+#                   ),
+#                 },
+#               ),
+#             },
+#           ),
