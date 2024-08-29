be2e98e78eef3759254414031283158afb6ae6ce
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-08-29 21:20:35 +0630

    back to the past

--- DualModelApp/components/image_detection.py
+++ DualModelApp/components/image_detection.py
@@ -5,15 +5,50 @@ from io import BytesIO  # For handling byte streams (e.g., file uploads)
 import streamlit as st  # For creating Streamlit apps
 
 from PIL import Image  # For image processing
-from dotenv import load_dotenv  # For loading environment variables
-
 import google.generativeai as genai  # For using the Gemini AI model
 
+
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+SCOPES = ['https://www.googleapis.com/auth/generative-language.retriever']
+
+def load_creds():
+    """Converts `client_secret.json` to a credential object.
+
+    This function caches the generated tokens to minimize the use of the
+    consent screen.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('X:\CURRENT PROJECTS\ChatBot_with_Streamlit\saves\\token.json'):
+        creds = Credentials.from_authorized_user_file('X:\CURRENT PROJECTS\ChatBot_with_Streamlit\saves\\token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'X:\CURRENT PROJECTS\ChatBot_with_Streamlit\saves\client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('X:\CURRENT PROJECTS\ChatBot_with_Streamlit\saves\\token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+
 # Load environment variables from .env file
-load_dotenv()
+creds = load_creds()
 
 # Configure the API key
-genai.configure(api_key=os.environ["GEMINI_AI_API_KEY"])
+genai.configure(credentials=creds)
 
 # Import your model (replace this with your actual model instance)
 GEMINI_PRO_1O5 = genai.GenerativeModel('gemini-1.5-pro')
